a6ca5c9b6c0effd6055ac43273675786b143582d
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2021-03-29 19:35:01 +0700

    Add Find Maximum Value of Matrix Array

--- Array-Dua-Dimensi/program-1.2.cpp
+++ Array-Dua-Dimensi/program-1.2.cpp
@@ -7,16 +7,20 @@ using namespace std;
 int main()
 {
     int baris, kolom, matriks[10][10];
-    int i,j;
-
+    int i, j;
+    int angkaCari, angkaMaks;
 
     // Input elemen array secara Row Major Order
 
-    cout << "Masukkan jumlah baris = " ;
+    //Input jumlah baris
+    cout << "Masukkan jumlah baris = ";
     cin >> baris;
+
+    //Input jumlah kolom
     cout << "Masukkan jumlah kolom = ";
     cin >> kolom;
 
+    //input elemen array
     cout << "Input elemen Array : \n";
     for (i = 0; i < baris; i++)
     {
@@ -30,11 +34,35 @@ int main()
 
     // Tampilkan elemen Array secara Row Major Order
 
-    cout << "Isi array : \n";
+    cout << "Isi array Secara Row Major Order: \n";
     for (i = 0; i < baris; i++)
     {
         for (j = 0; j < kolom; j++)
             cout << "  " << matriks[i][j];
         cout << endl;
     }
+
+    // Tampilkan elemen Array secara Column Major Order
+    cout << "Isi array Column Major Order : \n";
+    for (j = 0; j < baris; j++)
+    {
+        for (i = 0; i < kolom; i++)
+            cout << "  " << matriks[i][j];
+        cout << endl;
+    }
+
+    // Mencari elemen maksimum array
+    angkaCari = 0;
+    for (i = 0; i < baris; i++)
+    {
+        for (j = 0; j < kolom; j++)
+        {
+            if (matriks[i][j] >= angkaCari)
+            {
+                angkaMaks = matriks[i][j];
+            }
+        }
+    }
+
+    cout << "Nilai Maksimum dari Array adalah " << angkaMaks << endl;
 }
