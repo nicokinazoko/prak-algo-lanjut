914c0941a895ed3e92bfbb4c1b3bf53ad6b71d2c
nicokinazoko <nicodunois017157@gmail.com>
2021-05-30 20:34:51 +0700

    Create program-6-6.cpp

---
+++ Pointer/program-6-6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    char *pkota = "YOGYAKARTA"; /* pkota menunjuk konstanta string
+"YOGYAKARTA" */
+    cout << pkota << endl;
+}
