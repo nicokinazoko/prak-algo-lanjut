17b92c21064ec04ee796a0cc57050858bf0b8c0b
nicokinazoko <nicodunois017157@gmail.com>
2021-05-30 20:35:02 +0700

    Create program-4-7.cpp

---
+++ Searching/program-4-7.cpp
@@ -0,0 +1,37 @@
+/* BinSearch_SudahUrut
+diasumsikan Array X sudah ada dan berisi data yang sudah terurut,
+nilai yang dicari adalah nilaiCari dan hanya ada satu */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int X[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int i, nilaiCari, j, k;
+    bool found;
+
+    cout << "nilai yang dicari = ";
+    cin >> nilaiCari;
+
+    found = false;
+    i = 0;
+    j = 10;
+    while ((!found) & (i <= j))
+    {
+        k = (i + j) / 2;
+        if (nilaiCari == X[k])
+            found = true;
+        else
+        {
+            if (nilaiCari < X[k])
+                j = k - 1; // i tetap
+            else
+                i = k + 1; // j tetap
+        }
+    }
+    if (found)
+        cout << nilaiCari << " ditemukan dalam Array pada index ke-" << k << endl;
+    else
+        cout << "tidak ada " << nilaiCari << " dalam Array" << endl;
+}
